c5910f1abf255176a343436e2668f67812fac68f
noah <noah.joyner@pm.me>
2024-04-09 21:59:40 -0400

    Moved zoxide line to bottom of fish config - did not work with nix os

--- fish/config.fish
+++ fish/config.fish
@@ -93,8 +93,8 @@ function fish_greeting
   neofetch_greeting
 end
 
-zoxide init fish | source
-
 if status is-interactive
   # Commands to run in interactive sessions can go here
 end
+
+zoxide init fish | source
