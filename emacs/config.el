250b0506ce896e9767ea4b12b3d2a26e9e2c5ae9
noah <noah.joyner@pm.me>
2024-04-16 00:31:32 -0400

    Modified emacs font size
    that's it... may make this system level

--- emacs/config.el
+++ emacs/config.el
@@ -202,7 +202,7 @@
   :slant 'italic)
 
 ;;add font to default
-(add-to-list 'default-frame-alist '(font . "FiraCode-12"))
+(add-to-list 'default-frame-alist '(font . "FiraCode-11"))
 
 ;;set line spacing
 (setq-default line-spacing 0.15)
